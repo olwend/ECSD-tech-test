221e4dd48c34cb1fdc2ce3143896020f07a80b38
olwend <olwend@gmail.com>
2018-10-23 18:29:29 +0100

    function to return a running sum

--- src/test/e2e/intro.test.js
+++ src/test/e2e/intro.test.js
@@ -1,10 +1,54 @@
 /**
  * @jest-environment jest-environment-webdriver
  */
- function midIndex(array){
-   var lastIndex = array.length - 1;
-   return lastIndex;
- };
+
+function getTotal(array) {
+  var Total = array.reduce((a, b) => a + b, 0);
+  return Total;
+};
+
+function loopLR(array) {
+  var runningSum = 0;
+  var x;
+  var array_LR = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (i == 0) {
+      array_LR[0] = array[0];
+    } else {
+      x = i;
+      runningSum = array[x];
+      while (x > 0) {
+        x--;
+        runningSum = runningSum + array[x];
+      }
+      array_LR[i] = runningSum;
+    }
+  }
+  console.log('arrayLR: ' + array_LR);
+  return array_LR;
+}
+
+function loopRL(array) {
+  var runningSum = 0;
+  var x;
+  var array_RL = [];
+  for (var i = array.length - 1; i > -1; i--) {
+    if (i == array.length - 1) {
+      array_RL[i] = array[i];
+    } else {
+      x = i;
+      runningSum = array[x];
+      while (x < array.length - 1) {
+        x++;
+        runningSum = runningSum + array[x];
+      }
+      array_RL[i] = runningSum;
+    }
+  }
+  console.log('arrayRL: ' + array_RL);
+  return array_RL;
+}
 
 var array1 = new Array;
 var array2 = new Array;
@@ -64,48 +108,64 @@ describe('Data table scrape into arrays', () => {
   });
 });
 
+describe('low level running sum function tests', () => {
 
-describe ('low level function tests', () => {
+  test('getTotal gives sum of array items', async () => {
+    var array = [4, 4, 4];
+    var total = getTotal(array);
+    expect(total).toEqual(12);
+  });
 
-  test ('it returns index key not value', async () => {
-    var array = [4,4,4];
-    const lastIndex = midIndex(array);
-    expect(lastIndex).toEqual(2);
+  test('iterate from L -> R and each element returns a runningSum', async () => {
+    var array = [5, 1, 1, 1, 1, 1, 1];
+    var array_LR=loopLR(array);
+    console.log('arrayLR: ' + array_LR);
+  });
+
+  test('iterate from R -> L and each element returns a runningSum', async () => {
+    var array = [5, 1, 1, 1, 1, 1, 1];
+    var array_RL =loopRL(array);
+    console.log('arrayRL: ' + array_RL);
+  });
+
+
+
+  // test('at each loop it checks accum for equal both sides of the value', async () {
+  //   // loop and for each accum do a checks
+  //   // if lefthand sum === total - (lefthandsum + (array[i +1])
+  //   // THEN declare key i to be mid index
+  //   // send that as answer ....  set answer1 for array1, answer2 for array2 etc..
+
+
+});
+
+describe('submit answers', () => {
+  test('it can input answer1', async () => {
+    const ans1Input = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[1]/input'));
+    await ans1Input.sendKeys(xxx);
+  });
+
+  test('it can input answer2', async () => {
+    const ans2Input = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[2]/input'));
+    await ans2Input.sendKeys(answer2);
+  });
+
+  test('it can input answer3', async () => {
+    const ans3Input = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[3]/input'));
+    await ans3Input.sendKeys(answer3);
+  });
+
+  test('it can input fullName', async () => {
+    const nameInput = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[4]/input'));
+    await nameInput.sendKeys(fullName);
+  });
+
+  test('it can submit input answers via button', async () => {
+    const submitButton = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[2]/button'));
+    await submitButton.click();
   });
 });
 
-describe ('submit answers', () => {
-  test ('it can input answer1', async () => {
-  const ans1Input = await browser.findElement
-  (by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[1]/input'));
-  await ans1Input.sendKeys(answer1);
-  });
-
-  test ('it can input answer2', async () => {
-  const ans2Input = await browser.findElement
-  (by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[2]/input'));
-  await ans2Input.sendKeys(answer2);
-  });
-
-  test ('it can input answer3', async () => {
-  const ans3Input = await browser.findElement
-  (by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[3]/input'));
-  await ans3Input.sendKeys(answer3);
-  });
-
-  test ('it can input fullName', async () => {
-  const nameInput = await browser.findElement
-  (by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[4]/input'));
-  await nameInput.sendKeys(fullName);
-  });
-
-  test ('it can submit input answers via button', async () => {
-  const submitButton = await browser.findElement
-  (by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[2]/button'));
-  await submitButton.click();
-  });
+afterAll(() => {
+  return browser.quit();
 });
-
-  afterAll(() => {
-    return browser.quit();
-  });
