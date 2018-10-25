062b9c6480012004393bf212d7e44c5efc56aef7
olwend <olwend@gmail.com>
2018-10-25 09:16:37 +0100

    findAnswer() works but tests red as answers undefined outside of ()

--- src/test/e2e/intro.test.js
+++ src/test/e2e/intro.test.js
@@ -28,7 +28,6 @@ function loopLR(array) {
     }
   }
   console.log('arrayLR: ' + array_LR);
-
   return array_LR;
 }
 
@@ -50,41 +49,39 @@ function loopRL(array) {
     }
   }
   console.log('arrayRL: ' + array_RL);
-
   return array_RL;
 }
 
 function convertStoNum(array) {
-  for (var i=0; i<array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     array[i] = +array[i];
   }
 }
 
-function findAnswer(array) {
+function findAnswer(array, row) {
   var arrayLR = loopLR(array);
   var arrayRL = loopRL(array);
-  var answerIndex = null;
+  const answers = [];
 
   for (var i = 0; i < array.length; i++) {
     if (arrayLR[i] == arrayRL[i]) {
-      answerIndex = i;
+        answers[row-1] =i;
+
+      console.log('answer: ' + answers[row -1]);
       break;
     }
   }
-  console.log('answer: ' + answerIndex);
-  return answerIndex;
+  return answers[row-1];
 }
 // tests
 
 var array1 = new Array;
 var array2 = new Array;
 var array3 = new Array;
-var answerIndex;
-var answer1 = " ";
-var answer2 = " ";
-var answer3 = " ";
+const answers = [null, null, null];
+const row = row;
 
-jest.setTimeout(15000);
+jest.setTimeout(20000);
 
 var webdriver = require("selenium-webdriver");
 var browser = new webdriver.Builder().usingServer()
@@ -95,7 +92,7 @@ var browser = new webdriver.Builder().usingServer()
 const url = 'http://localhost:3000/';
 const fullName = "Olwen Davies";
 
-describe('ECS Intro page renders', () => {
+describe('ECS Intro page', () => {
   test('it has ECSDigital in h1', async () => {
     await browser.get(url);
     const title = await browser.findElement(by.tagName('h1')).getText();
@@ -111,74 +108,107 @@ describe('ECS Intro page renders', () => {
 
 describe('Iterate to create running sum arrays', () => {
 
-  test('getTotal gives sum of array items', async () => {
+  xtest('getTotal gives sum of array items', async () => {
     var array = [4, 4, 4];
     var total = getTotal(array);
     expect(total).toEqual(12);
   });
 
-  test('iterate from L -> R and each element returns a runningSum', async () => {
+  xtest('iterate from L -> R and each element returns a runningSum', async () => {
     var array = [5, 1, 1, 1, 1, 1, 1];
     var array_LR = loopLR(array);
-    console.log('arrayLR: ' + array_LR);
+    expect(array_LR).toContain(7);
   });
 
-  test('iterate from R -> L and each element returns a runningSum', async () => {
+  xtest('iterate from R -> L and each element returns a runningSum', async () => {
     var array = [5, 1, 1, 1, 1, 1, 1];
     var array_RL = loopRL(array);
-    console.log('arrayRL: ' + array_RL);
+    expect(array_RL).toContain(11);
   });
 });
 
-describe('Data table scrape into arrays and run function', () => {
-  test('read in tr1 gives array1 length as per 9 cols and & call findAnswer', async () => {
+describe('findAnswer() test cases', () => {
+  test('it shows null when there is no match', async () => {
+    const tcnull = [1,2,2,1,3,13,3];
+    console.log(tcnull);
+    findAnswer(tcnull,1);
+    expect(answers[row-1]).toEqual(null);
+  });
+
+  test('it handles zeroes when there is no match', async () => {
+    const tcnull = [0,0,0,0,0,0,0];
+    console.log(tcnull);
+    findAnswer(tcnull,1);
+    expect(answers[row-1]).toEqual(null);
+  });
+
+  test('it handles decimals', async () => {
+    const tcnull = [1.0,2.0,3.0,1.0,3.0,3.0];
+    console.log(tcnull);
+    findAnswer(tcnull,1);
+    expect(answers[row-1]).toEqual(3);
+  });
+
+  test('it handles decimals < 1', async () => {
+    const tcnull = [2.0,1.0,0.2,0.8,0.3,0.7];
+    console.log(tcnull);
+    findAnswer(tcnull,1);
+    expect(answers[row-1]).toEqual(1);
+  });
+});
+
+
+describe('Data table scrape', () => {
+  xtest('it reads in tr1 gives array1 length as per 9 cols and & call findAnswer', async () => {
 
     const tablerow1 = await browser.findElement(by.xpath('//tr[@id="tr1"]')).getText();
     array1 = tablerow1.split(" ");
     convertStoNum(array1);
     expect(array1.length).toBe(9);
     console.log(array1);
-    findAnswer(array1);
-    answer1 = answerIndex;
+    findAnswer(array1,1);
   });
 
-  test('read in tr2 gives array2 length as per 9 cols & call findAnswer', async () => {
+  xtest('it reads in tr2 gives array2 length as per 9 cols & call findAnswer', async () => {
 
     const tablerow2 = await browser.findElement(by.xpath('//tr[@id="tr2"]')).getText();
     array2 = tablerow2.split(" ");
     convertStoNum(array2);
     expect(array2.length).toBe(9);
     console.log(array2);
-    findAnswer(array2);
-    answer2 = answerIndex;
+    findAnswer(array2,2);
   });
 
-  test('read in tr3 gives array3 length as per 9 cols & call findAnswer', async () => {
+  xtest('it reads in tr3 gives array3 length as per 9 cols & call findAnswer', async () => {
 
     const tablerow3 = await browser.findElement(by.xpath('//tr[@id="tr3"]')).getText();
     array3 = tablerow3.split(" ");
     convertStoNum(array3);
     expect(array3.length).toBe(9);
     console.log(array3);
-    findAnswer(array3);
-    answer3 = answerIndex;
+    findAnswer(array3,3);
   });
 });
 
-describe('submit answers', () => {
+describe('Answers', async () => {
+//
+//   test('it has answers in an array', async () => {
+//     console.log(answers);
+//   });
+
   xtest('it can input answer1', async () => {
     const ans1Input = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[1]/input'));
-    await ans1Input.sendKeys(answer1);
+    await ans1Input.sendKeys(answers[0]);
   });
 
   xtest('it can input answer2', async () => {
     const ans2Input = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[2]/input'));
-    await ans2Input.sendKeys(answer2);
+    await ans2Input.sendKeys(answers[1]);
   });
 
   xtest('it can input answer3', async () => {
     const ans3Input = await browser.findElement(by.xpath('//*[@id="challenge"]/div/div/div[2]/div/div[1]/div[3]/input'));
-    await ans3Input.sendKeys(answer3);
+    await ans3Input.sendKeys(answers[2]);
   });
 
   xtest('it can input fullName', async () => {
